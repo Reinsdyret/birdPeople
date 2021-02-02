4131ed6bc225d422d4242627caeaae5ae82b2eac
Lars Møen Haukland <Lars.haukland@gmail.com>
2021-02-02 14:36:27 +0100

    idfk

--- code.js
+++ code.js
@@ -6,17 +6,17 @@ class Bird{
     direction;
     spritesheet;
     img;
-    mirrored = false;
+    side;
     constructor(x,y,xDirection,yDirection,spritesheet){
         this.x = x;
         this.y = y; 
         this.direction = {
-            x: xDirection,
-            y: yDirection
+            x: xDirection * -1,
+            y: yDirection * -1
         };
         this.spritesheet = spritesheet;
         this.img = document.createElement("img");
-        this.img.src = "sprites/" + this.spritesheet + ".png";
+        this.img.src = "sprites/" + spritesheet + ".png";
 
         //Sett høyde og bredde
         this.width = 800 / 4;
@@ -31,38 +31,28 @@ class Bird{
         if(this.y + this.height >= canvas.height || this.y <=0){
             this.direction.y *= -1;
         }
-        if(this.x + this.width >= canvas.width || this.x <= 0){
-            this.direction.x *= -1;
-            //Sets mirrored to opposite value of current (flips)
-            this.mirrored = !this.mirrored;
+        if(this.x <= 0 || this.x + this.width >= canvas.width){
+            this.xDirection *= -1;
         }
     }
 
     drawFrame(ctx,scale,frameX, frameY, canvasX, canvasY){
-        if(!this.mirrored){
-            ctx.drawImage(this.img,frameX * this.width, frameY * this.height, this.width, this.height, canvasX, canvasY, this.width * scale, this.height * scale);
-        }else{
-            console.log(mirrored);
-            ctx.save();
-            ctx.transform(-1,0,0,1,0,0);
-            ctx.drawImage(this.img,
-                frameX * this.width, frameY * this.height, this.width,this.height,
-                this.x, this.y, this.width * scale * -1, this.height * scale);
-            ctx.restore();
+            ctx.drawImage(this.img,frameX * this.width, frameY * this.height, this.width, this.height, this.x, this.y, this.width * scale, this.height * scale);
         }
         
     }
-
-
-
-}
 
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 canvas.style.border = "1px solid black";
 
 let birds = [];
-birds.push(new Bird(0,0,1,1,"lars"));
+birds.push(new Bird(0,200,0,-1,"tobiasReal"));
+birds[0].side = 2;
+birds.push(new Bird(200,200,0,1,"lars"));
+birds[1].side = 0;
+birds.push(new Bird(300,200,0,-1,"haakon"))
+birds[2].side = 1;
 
 
 
@@ -73,14 +63,17 @@ let startTime;
 let deltaTime = 0;
 let cycleGoingDown = false;
 let framesPerSecond = 10;
-function step(frameIndex, mirrored){
+function step(frameIndex){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     for(let i = 0; i<birds.length; i++){
-        birds[i].drawFrame(ctx,scale,cycleLoop[frameIndex],2,0,0,mirrored);
+        birds[i].drawFrame(ctx,scale,cycleLoop[frameIndex],birds[i].side,0,0);
     }
 }
 
 function loop(){
+    for(let i = 0; i<birds.length; i++){
+        birds[i].update();
+    }
     deltaTime = (new Date().getTime() / 1000) - startTime ;
     if(Math.floor(frameIndex) > 3){
         cycleGoingDown = true;
@@ -90,7 +83,7 @@ function loop(){
     }
     frameIndex += deltaTime * framesPerSecond;
     ctx.scale(1,-1);
-    step(Math.floor(frameIndex),mirror);
+    step(Math.floor(frameIndex));
     
 
     if(Math.floor(frameIndex) <= 0){
@@ -98,6 +91,8 @@ function loop(){
     }
     startTime = new Date().getTime() / 1000;
     window.requestAnimationFrame(loop);
+
+    
 }
 
 function init(){
